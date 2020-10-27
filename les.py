413c9f68d8160755d1c5848c77a2bd27fa4c75ba
Saidolim <you@example.com>
2020-10-27 17:21:37 +0500

    first commit

---
+++ les.py
@@ -0,0 +1,42 @@
+# # pi=3.14
+# # L=float(input('L:'))
+# # print("Radius =",L/(2*pi))
+# # print("ay;ana yuzasi",pi*(L/2*pi)**2)
+# x1=float(input("x1="))
+# x2=float(input("x2="))
+# if x1!=x2:
+#     print(abs(x1-x2))
+# elif x1==x2:
+#     print("nuqtalar bir joyda")
+#import math
+
+# 25
+# import random
+# x= random.randrange(-10,10)
+# print("x=",x)
+# y=3*x**6-6*x**2-7
+# print("funksiya natijasi",y)
+# print("3*{0}^6-6*{0}^2-7)= {1}".format(x,y))
+
+# 9
+# a=float(input("a="))
+# b=float(input("b="))
+# if a>0 and b>0:
+#     print((a*b)**(1/2))
+# elif a>0 and b>0 or a<0 and b>0 :
+#     print ('xuy javob ciqadi')
+# else:
+#     print("ishladiku pidr")
+
+#shart opperatorlari
+# import random
+# r=list(range(-3,-1))+list (range(1,3))
+# a=random.choice(r)
+# print(a)
+
+a=float
+(input("a:"))
+pi = 3.15
+pi = 3.14
+print(pi)
+print(a%pi)
